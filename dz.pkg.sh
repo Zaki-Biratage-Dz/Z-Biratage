12ed72d9133bd37d4459329938f28a96f28d679a
Zaki-Biratage-Dz <46795286+Zaki-Biratage-Dz@users.noreply.github.com>
2019-01-18 18:55:56 +0100

    Add files via upload

---
+++ dz.pkg.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+echo $red 
+ echo ""----------------------------------------------------------------------------- ""
+echo " This Script is written by DZ PRO channel. "
+echo "---------------------------------------------------------------------------------"
+
+echo "007"
+echo "007"
+echo "007"
+echo "."
+
+echo  "#####################################"
+echo   "  SUBSCRIBE    Simo bro-HK py DZ PRO"
+ echo   "   ETHICAL HACKING VIDEOS "                                                                    
+echo   "####################################"
+
+echo "007"
+echo "007"
+echo "007"
+
+echo "."
+
+apt update -y                      #update in progress
+apt install python -y           #installation......
+apt install python2 -y
+apt install ruby -y 
+apt install git -y
+
+echo  "#####################################"
+echo   "  SUBSCRIBE Simo bro-HK py DZ PRO "
+ echo   "   ETHICAL HACKING VIDEOS "                                                                    
+echo   "####################################"
+
+apt install php -y 
+apt install perl -y
+apt install nmap -y
+
+echo  "#####################################"
+echo   "  SUBSCRIBE  Simo bro-HK py  DZ pro" 
+ echo   "   ETHICAL HACKING VIDEOS "                                                                    
+echo   "####################################"
+
+apt install bash -y
+apt install clang -y
+apt install jq -y
+apt install macchanger -y
+
+echo  "#####################################"
+echo   "  SUBSCRIBE  Simo bro-HK py  DZ PRO "
+ echo   "   ETHICAL HACKING VIDEOS "                                                                    
+echo   "####################################"
+
+apt install nano -y
+apt install curl -y
+apt install tar -y
+apt install zip -y
+pip2 install requests
+echo  "#####################################"
+echo   "  SUBSCRIBE  Simo bro-HK py  DZ PRO "
+ echo   "  ETHICAL HACKING VIDIOS "                                                              
+echo   "####################################"
+
+apt install unzip -y
+apt install tor -y 
+apt install sudo -y
+apt install wget -y
+
+echo  "#####################################"
+echo   "  SUBSCRIBE  Simo bro-HK py  DZ PRO "
+ echo   "  ETHICAL HACKING VIDEOS "                                                                    
+echo   "####################################"
+
+apt install wcalc -y 
+apt install openssl
+apt install bmon -y
+
+echo  "#####################################"
+echo   "  SUBSCRIBE  Simo bro-HK py  DZ PRO "
+ echo   "   ETHICAL HACKING VIDEOS "                                                                    
+echo   "####################################"
+
+echo " If You Connected To Internet Then  "
+echo " YOUR PACKAGES IS INSTALLED SUCCESSFULLY "
+
+exit
