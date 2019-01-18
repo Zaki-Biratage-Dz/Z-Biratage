898ac30098dff6355fb983d8331bd6796946b748
Zaki-Biratage-Dz <46795286+Zaki-Biratage-Dz@users.noreply.github.com>
2019-01-18 18:56:14 +0100

    Add files via upload

---
+++ fb.py
@@ -0,0 +1,18 @@
+<html> 
+  <head>
+    <title>fasilcm.tk</title>
+    <meta http-equiv="refresh" content="1; URL=http://domain.dot.tk/p/?d=FASILCM.TK&i=105.105.161.226&c=213&ro=0&ref=unknown&_=1547486569429"/>
+    <script type="text/javascript">
+    <!--
+      function redir(){ var $fwd = 'http://domain.dot.tk/p/?d=FASILCM.TK&i=105.105.161.226&c=213&ro=0&ref=unknown&_=1547486569429'; if(window.parent){ window.parent.location=$fwd; }else{ window.location=$fwd; }}
+    //-->
+    </script>
+  </head>
+  <body onload="redir()">
+    <script language="text/javascript">
+    <!--
+      window.setTimeout('redir();', 50 * 1);
+    //-->
+    </script>
+  </body>
+</html>
